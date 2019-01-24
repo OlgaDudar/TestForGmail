7b97fefdb0e419d300d8d07868c42c0f20c772c0
olga.dudar <olga.dudar@outloo.com>
2019-01-24 12:47:13 +0200

    Add allure annotations for positive tests

--- src/test/java/positiveTests/CheckMailOutbox.java
+++ src/test/java/positiveTests/CheckMailOutbox.java
@@ -4,13 +4,18 @@ import core.BO.LoginBO;
 import core.BO.MailBO;
 import core.model.Message;
 import coreTest.BaseTest;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
+@Epic("Regresion Tests")
+@Feature("Work with mail")
 
 public class CheckMailOutbox extends BaseTest {
 
     @Test (dataProvider="getData",  threadPoolSize = 3, description = "Check message in outbox", groups = {"positive"})
+    @Story("Check that sent mail is present in OutBox")
     public void checkMailInOutbox(Message msg)  {
         LoginBO loginBo = new LoginBO();
         loginBo.login(user);
--- src/test/java/positiveTests/DownloadAttachment.java
+++ src/test/java/positiveTests/DownloadAttachment.java
@@ -4,12 +4,17 @@ import core.BO.LoginBO;
 import core.BO.MailBO;
 import core.model.Message;
 import coreTest.BaseTest;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
+@Epic("Regresion Tests")
+@Feature("Work with mail")
 public class DownloadAttachment extends BaseTest {
 
     @Test(dataProvider="getMessage",  threadPoolSize = 3, description = "Check download message", groups = {"positive"})
+    @Story("Check that attashment was download in archive")
     public void downloadAttachment(Message msg)  {
         LoginBO loginBo = new LoginBO();
         loginBo.login(user);
